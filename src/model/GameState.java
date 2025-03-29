638a2e28a8686b8cff21ab863749426e4ab1e6b0
veradescent <148369761+veradescent@users.noreply.github.com>
2025-03-29 21:33:37 +0800

    Update GameState.java

--- src/model/GameState.java
+++ src/model/GameState.java
@@ -1,5 +1,43 @@
 package model;
 
+import java.util.ArrayList;
+import model.board.*;
+
 public class GameState {
-    
+    private Board board;
+    private ArrayList<Player> players;
+    private Player currentPlayer;
+    private boolean gameOver;
+
+    public GameState(){
+        initializeGame();
+    }
+
+    public Board getBoard(){
+        return this.board;
+    }
+
+    public Player getCurrentPlayer(){
+        return this.currentPlayer;
+    }
+
+    public ArrayList<Player> getPlayers(){
+        return this.players;
+    }
+
+    public boolean isGameOver(){
+        return this.gameOver;
+    }
+
+    public void resetGame(){
+        initializeGame();
+    }
+
+    private void initializeGame(){
+        this.players = new ArrayList<Player>();
+        this.players.add(new Player("Player 1"));
+        this.players.add(new Player("Player 2"));
+        this.board = new Board(players);
+        this.currentPlayer = this.players.get(0);
+    }
 }
