f2f3df46a17d2e7508a3a93f9af17c58b4fa2c82
veradescent <148369761+veradescent@users.noreply.github.com>
2025-04-02 01:16:32 +0800

    Cleaned up the code

--- src/model/pieces/Lion.java
+++ src/model/pieces/Lion.java
@@ -2,13 +2,12 @@ package model.pieces;
 
 import model.Player;
 
-public class Lion extends Piece implements Swimmer{
-    public Lion(int row, int col, Player owner){
-        super("Lion", 7, row, col, owner);
+public class Lion extends Piece implements Swimmer {
+    public Lion(Player owner){
+        super("Lion", 7, owner);
     }
 
     public boolean crossLake(){
         return true;
     }
-
 }
