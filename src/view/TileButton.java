b380cbb086a1cba03e795f20ab5f3e35b3ea6343
veradescent <148369761+veradescent@users.noreply.github.com>
2025-04-01 21:37:35 +0800

    Fixed display of HomeBase and Trap

--- src/view/TileButton.java
+++ src/view/TileButton.java
@@ -12,9 +12,6 @@ public class TileButton extends JButton {
 
     private static final Color REGULAR_TILE_COLOR = Color.WHITE;
     private static final Color LAKE_COLOR = Color.BLUE;
-    private static final Color TRAP_COLOR = Color.GRAY;
-    private static final Color HOMEBASE_1_COLOR = new Color(255,102,102);
-    private static final Color HOMEBASE_2_COLOR = new Color(51,204,255);
     private static final Color PLAYER_1_COLOR = new Color(255,102,102);
     private static final Color PLAYER_2_COLOR = new Color(51,204,255);
 
@@ -25,7 +22,7 @@ public class TileButton extends JButton {
         this.setBackground(this.getTileColor());
     }
 
-    public Tile getTile(){
+    public Tile getTile(){ // remove?
         return this.tile;
     }
 
@@ -33,6 +30,13 @@ public class TileButton extends JButton {
         Piece piece = tile.getCurrentPiece();
         if (piece != null){
             setPieceIcon(piece);
+        } else if (tile instanceof model.board.HomeBase && ((model.board.HomeBase)tile).getOwner().getName().equals("Player 1")){
+            setTileIcon("Player 1");
+        } else if (tile instanceof model.board.HomeBase && ((model.board.HomeBase)tile).getOwner().getName().equals("Player 2")){
+            setTileIcon("Player 2");
+        } else if (tile instanceof model.board.Trap){
+            setTileIcon("Trap");
+            this.setBackground(this.getTileColor());
         } else {
             setIcon(null);
             this.setBackground(this.getTileColor());
@@ -60,15 +64,21 @@ public class TileButton extends JButton {
         }
     }
 
+    private void setTileIcon(String tileName){
+        String path = "resources\\images\\" + pieceName + ".png";
+        try {
+            ImageIcon icon = new ImageIcon(path);
+
+            Image scaledImage = icon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+            setIcon(new ImageIcon(scaledImage));
+        } catch (Exception e){
+            System.err.println("Error loading image: " + path);
+        }
+    }
+
     private Color getTileColor(){
         if (tile instanceof model.board.Lake){
             return LAKE_COLOR;
-        } else if (tile instanceof model.board.Trap){
-            return TRAP_COLOR;
-        } else if (tile instanceof model.board.HomeBase && ((model.board.HomeBase)tile).getOwner().getName().equals("Player 1")){
-            return HOMEBASE_1_COLOR;
-        } else if (tile instanceof model.board.HomeBase && ((model.board.HomeBase)tile).getOwner().getName().equals("Player 2")){
-            return HOMEBASE_2_COLOR;
         } else {
             return REGULAR_TILE_COLOR;
         }
