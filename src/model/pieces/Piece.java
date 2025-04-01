6a1fe1f6fbbb8883311e616b6c63626006d10693
veradescent <148369761+veradescent@users.noreply.github.com>
2025-04-01 21:00:50 +0800

    Update Piece.java

--- src/model/pieces/Piece.java
+++ src/model/pieces/Piece.java
@@ -7,6 +7,7 @@ public abstract class Piece {
     protected int strength;
     protected int row, col;
     protected Player owner;
+    protected boolean trapped;
 
     public Piece(String name, int strength, int row, int col, Player owner){
         this.name = name;
@@ -14,6 +15,7 @@ public abstract class Piece {
         this.row = row;
         this.col = col;
         this.owner = owner;
+        this.trapped = false;
     }
 
     public String getName(){
@@ -28,17 +30,46 @@ public abstract class Piece {
         return this.owner;
     }
 
-    public boolean canCapture(Piece target){ // check
+    public boolean isTrapped(){
+        return this.trapped;
+    }
+
+    public boolean canCapture(Piece target){
         boolean flag = this.strength >= target.getStrength();
         if (target.getOwner() == this.owner){
             flag = false;
+        } 
+
+        if (this instanceof Rat && target instanceof Elephant){ // exception
+            flag = true;
         }
 
         return flag;
     }
 
-    public void move(int newRow, int newCol){ // haven't implemented this yet
-        this.row = newRow;
-        this.col = newCol;
+    public void setTrapped(boolean trapped){
+        this.trapped = trapped;
+
+        if (trapped == true){
+            this.strength = 0;
+        } else if (trapped == false){ // i'll revise this since this is brute force palang
+            if (this instanceof Elephant){
+                this.strength = 8;
+            } else if (this instanceof Lion){
+                this.strength = 7;
+            } else if (this instanceof Tiger){
+                this.strength = 6;
+            } else if (this instanceof Leopard){
+                this.strength = 5;
+            } else if (this instanceof Wolf){
+                this.strength = 4;
+            } else if (this instanceof Dog){
+                this.strength = 3;
+            } else if (this instanceof Cat){
+                this.strength = 2;
+            } else if (this instanceof Rat){
+                this.strength = 1;
+            }
+        }
     }
 }
