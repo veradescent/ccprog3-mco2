9d6dbcf9ded2130825f46afeca8355e5f833c740
veradescent <148369761+veradescent@users.noreply.github.com>
2025-04-02 01:14:45 +0800

    Cleaned up the code

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,26 +1,13 @@
 package model;
 
-import java.util.ArrayList;
-import model.pieces.*;
-
 public class Player {
     private String name;
-    private ArrayList<Piece> pieces;
 
     public Player(String name){
         this.name = name;
-        this.pieces = null;
     }
 
     public String getName(){
         return this.name;
-    }
-
-    public void setPieces(ArrayList<Piece> pieces){
-        this.pieces = pieces;
-    }
-
-    public ArrayList<Piece> getPieces(){
-        return this.pieces;
     }
 }
