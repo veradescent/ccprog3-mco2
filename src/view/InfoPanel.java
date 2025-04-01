5bde38c42eaaec053754df023b75bf5f080d6371
veradescent <148369761+veradescent@users.noreply.github.com>
2025-04-01 21:07:25 +0800

    Update InfoPanel.java

--- src/view/InfoPanel.java
+++ src/view/InfoPanel.java
@@ -4,10 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 
 public class InfoPanel extends JPanel {
-    private JLabel playerTurnLbl;
-
     public InfoPanel(){
-        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        this.setPreferredSize(new Dimension(200,600));
+        this.setLayout(new FlowLayout());
+        this.setPreferredSize(new Dimension(200,100));
     }
 }
