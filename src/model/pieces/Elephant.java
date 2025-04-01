af2e47b58ddbc647047b80cd25822a97ca0c88a6
veradescent <148369761+veradescent@users.noreply.github.com>
2025-04-02 01:16:05 +0800

    Update Elephant.java

--- src/model/pieces/Elephant.java
+++ src/model/pieces/Elephant.java
@@ -3,7 +3,7 @@ package model.pieces;
 import model.Player;
 
 public class Elephant extends Piece {
-    public Elephant(int row, int col, Player owner){
-        super("Elephant", 8, row, col, owner);
+    public Elephant(Player owner){
+        super("Elephant", 8, owner);
     }
 }
