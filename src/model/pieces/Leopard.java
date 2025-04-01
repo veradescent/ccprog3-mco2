afcd7530448f53c1af673fd9531232a5b7f10c04
veradescent <148369761+veradescent@users.noreply.github.com>
2025-04-02 01:17:14 +0800

    Cleaned up the code

--- src/model/pieces/Leopard.java
+++ src/model/pieces/Leopard.java
@@ -2,8 +2,8 @@ package model.pieces;
 
 import model.Player;
 
-public class Leopard extends Piece{
-    public Leopard(int row, int col, Player owner){
-        super("Leopard", 5, row, col, owner);
+public class Leopard extends Piece {
+    public Leopard(Player owner){
+        super("Leopard", 5, owner);
     }
 }
