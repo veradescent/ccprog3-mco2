fb010a0055aa08099fbabf04fe6c4834f8fc91e0
veradescent <148369761+veradescent@users.noreply.github.com>
2025-04-02 01:17:38 +0800

    Cleaned up the code

--- src/model/pieces/Wolf.java
+++ src/model/pieces/Wolf.java
@@ -3,7 +3,7 @@ package model.pieces;
 import model.Player;
 
 public class Wolf extends Piece {
-    public Wolf(int row, int col, Player owner){
-        super("Wolf", 4, row, col, owner);
+    public Wolf(Player owner){
+        super("Wolf", 4, owner);
     }
 }
